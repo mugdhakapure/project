24009ca2072f0bffe0b9380f11663eca62ddee49
mugdhakapure <98604319+mugdhakapure@users.noreply.github.com>
2022-02-01 22:31:24 +0530

    This is  my 1st project

---
+++ Java/Phase1/Phase1java/src/typecasting.java
@@ -0,0 +1,20 @@
+//WAP which will take a string as input and will convert it into other primitive data types.
+public class typecasting {
+
+	public static void main(String[] args)
+	{
+	String str = "5555";
+	
+	System.out.println(Integer.valueOf(str));
+	
+	System.out.println(Long.valueOf(str));
+	
+	System.out.println(Boolean.valueOf(str));
+	
+	System.out.println(Double.valueOf(str));
+	
+	System.out.println(Float.valueOf(str));
+
+	}
+
+}
